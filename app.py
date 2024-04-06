4599777c8ec69a24827306acc953740e4bb594c7
Fin-X-Admin <164788719+Fin-X-Admin@users.noreply.github.com>
2024-04-06 11:04:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,63 @@
 import os
 import re
 import streamlit as st
+
+# Google sheets as DB
+
+import pandas as pd
+from streamlit_gsheets import GSheetsConnection
+
+
+#############
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+
+from langchain_community.chat_models import ChatOpenAI
 from decouple import config
 
-#And greet the user thanking for using the service after the budget plan is served. You are a finance advisor AI agent and your aim is to ask questions from user to develop a budgeting plan, 
-              # Question user in a consultant tone and get only the details of age, marital status, annual income, cost of living and devise
-              # a budgeting plan after the information is retrieved. And make a planning for monthly budget.
-
-os.environ['OPENAI_API_KEY'] = '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'
 
 
-def extract_user_info(prompt1):
+os.environ['OPEN_API_KEY'] = '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'
+
+
+
+
+
+# Function to extract user information from the prompt
+def extract_user_info(arg):
     user_info = {}
 
-    print(prompt1)
+    print(user_info)
     # Extract annual income
-    income_match = re.search(r'annual_income: (\d+)', prompt1, re.IGNORECASE)
+    income_match = re.search(r'"annual_income": (\d+)', arg, re.IGNORECASE)
     if income_match:
-        user_info['annual_income'] = int(income_match.group(1))
+        user_info['Annual Income ₹'] = int(income_match.group(1))
   
     #Extracting Martal Status 
-    marital_status_match = re.search(r'marital_status: (\w+)', prompt1, re.IGNORECASE)
+    marital_status_match = re.search(r'marital_status: (\w+)', arg, re.IGNORECASE)
+    marital_status_match = re.search(r'"marital_status": (\w+)', arg, re.IGNORECASE)
     if marital_status_match:
-        user_info['marital_status'] = marital_status_match.group(1)
+        user_info['Marital Status'] = marital_status_match.group(1)
     
     
     # Extract cost of living
-    cost_of_living_match = re.search(r'cost_of_living: (\d+)', prompt1, re.IGNORECASE)
+    cost_of_living_match = re.search(r'"cost_of_living": (\d+)', arg, re.IGNORECASE)
     if cost_of_living_match:
-        user_info['cost_of_living'] = int(cost_of_living_match.group(1))
+        user_info['Cost of Living ₹'] = int(cost_of_living_match.group(1))
     
     # Extract age
-    age_match = re.search(r'age: (\d+)', prompt1, re.IGNORECASE)
+    age_match = re.search(r'"age": (\d+)', arg, re.IGNORECASE)
     if age_match:
-        user_info['age'] = int(age_match.group(1))
-
+        user_info['Age'] = int(age_match.group(1))
 
     return user_info
 
 
+
 prompt1 = PromptTemplate(
-  input_variables = ["chat_history", "question"],
+  input_variables = ["chat_history", "human_input"],
   template="""You are a financial advisor to help user with budgeting plans. 
               Keep all of the currency in INR ₹
               Follow these steps 
@@ -53,19 +66,24 @@ prompt1 = PromptTemplate(
                   a. Annual Income 
                   b. Marital status 
                   c. Cost of living 
-                  d. age  
+                  d. Age 
 
+              
               2. If user has trouble figuring out cost of living ask general questions and give it a general amount yourself 
 
-              3. Once you have recieved all of these informations generate a budgeting plan for the next 5 years of this person for comfortable retirement plan by age 50 
+              3. Once you have received all of these information generate a budgeting plan for the next 5 years of this person for comfortable retirement plan by age 50 
               chat_history: {chat_history}
-              Human: {question} 
+              Human: {human_input}
+
 
               AI:"""
 )
 
+
+
+# for extracting user info
 prompt2 = PromptTemplate(
-  input_variables = ["chat_history", "question"],
+  input_variables = ["chat_history", "human_input"],
   template="""You are a financial advisor to help user with budgeting plans. 
               Keep all of the currency in INR ₹
               Follow these steps 
@@ -76,7 +94,7 @@ prompt2 = PromptTemplate(
                   c. Cost of living 
                   d. age 
 
-              all at once or step by step then just output a dictionry 
+              all at once or step by step then just output a dictionry and please make sure the money amount is all in numbers
               
                 annual_income: 
                 marital_status:
@@ -85,24 +103,26 @@ prompt2 = PromptTemplate(
 
 
               update keys if user updates any 
-
+            
               only reply with dictionary and nothing else 
               chat_history: {chat_history}
-              Human: {question} 
+              Human: {human_input} 
 
               AI:"""
 )
 
-llm = ChatOpenAI(openai_api_key = config("OPENAI_API_KEY"))
-memory = ConversationBufferMemory(memory_key="chat_history", k=50)
-memory2 = ConversationBufferMemory(memory_key="chat_history", k=50)
+llm = ChatOpenAI(openai_api_key = config("OPEN_API_KEY"),  temperature=0.6, model="gpt-4")  #model="gpt-4",
+
+
+memory = ConversationBufferMemory(memory_key="chat_history", llm=llm)
 llm_chain = LLMChain(
   llm=llm,
   memory=memory,
   prompt=prompt1,
-  
 )
 
+## LLM chain 2 used in prompt2
+memory2 = ConversationBufferMemory(memory_key="chat_history", llm=llm)
 llm_chain_2  = LLMChain(
   llm=llm,
   memory=memory2,
@@ -110,13 +130,13 @@ llm_chain_2  = LLMChain(
   
 )
 
+
 st.set_page_config(
-  page_title="Finance Planning Advice Bot",
+  page_title="FinX",
   page_icon="🤖",
   layout="wide"
 )
 
-# Fonts and style
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -126,7 +146,8 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-st.title("Finance Planning Advice Bot")
+
+st.title("FinX")
 
 if "messages" not in st.session_state.keys():
   st.session_state.messages = [
@@ -149,29 +170,28 @@ if user_prompt is not None:
 if st.session_state.messages[-1]["role"] != "assistant":
   with st.chat_message("assistant"):
     with st.spinner("Loading..."):
-      ai_response = llm_chain.predict(question=user_prompt)
-      ai_dict = llm_chain_2.predict(question=user_prompt)
+      ai_response = llm_chain.predict(human_input=user_prompt)
 
-      extracted_info = extract_user_info(ai_dict)
-      st.session_state.user_info.update(extracted_info)
-      
       st.write(ai_response)
+
+      ai_dict = llm_chain_2.predict(human_input=user_prompt)
+      print(ai_dict)
+      extracted_info = extract_user_info(ai_dict)
+      # print(extracted_info)
+
+      st.session_state.user_info.update(extracted_info)
+
+
   new_ai_message = {"role": "assistant", "content": ai_response}
   st.session_state.messages.append(new_ai_message)
 
 
-########################  
-
-
-
-
-# Extracting User info on the sidebar 
+###############################
+  
 
 # Initialize session state for user info if not already set
 if 'user_info' not in st.session_state:
     st.session_state.user_info = {}
-
-# Function to extract user information from the prompt
 
 
 # Collect user prompt from atop
@@ -183,11 +203,23 @@ if user_prompt:
     extracted_info = extract_user_info(user_prompt)
     st.session_state.user_info.update(extracted_info)
 
-# if ai_dict: 
-    
 # Display user info in the sidebar
 with st.sidebar:
     st.subheader("User Information")
     if 'user_info' in st.session_state and st.session_state.user_info:
         for key, value in st.session_state.user_info.items():
             st.write(f"{key.capitalize()}: {value}")
+
+
+
+#-------------------------------------------#
+
+
+
+# Establishing a Google Sheets connection
+# conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+# if st.button("New Worksheet"):
+#     conn.create(worksheet="Orders", data=extracted_info)
+#     st.success("Worksheet Created 🎉")
+
+#-------------------------------------------#

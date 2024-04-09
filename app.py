8e878523b7b7f8ed26d5e3e6b3035aae995804c2
Fin-X-Admin <164788719+Fin-X-Admin@users.noreply.github.com>
2024-04-09 12:50:06 +0530

    Update app.py (User input Data Updated in Google Sheet)

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import streamlit as st
 import json 
 from typing import List
 # Google sheets as DB
-# import pandas as pd
-# from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 #############
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
@@ -19,13 +19,13 @@ from decouple import config
 #parser
 class user_detail(BaseModel):
     # user_data: dict = Field(description="The output should only contain a JSON instance that conforms to the JSON schema below. Here is the output schema delimited by triple backticks: ```{'Annual_income': '20000', 'Marital_Status': 'Single', ...}```")
-    annual_income: int = Field(description="integer")
-    marital_status: str = Field(description="single or married")
-    cost_of_living: int = Field(description="Integer value")
-    age: int = Field(description="Integer value, User's age")
+    annual_income: int = Field(description="integer, default: 0")
+    marital_status: str = Field(description="single or married, default: NaN")
+    cost_of_living: int = Field(description="Integer value, default: 0")
+    age: int = Field(description="Integer value, User's age, default: 0")
     
 
-os.environ['OPEN_API_KEY'] = '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'
+# os.environ['OPEN_API_KEY'] = ''
 st.set_page_config(
   page_title="FinX",
   page_icon="🤖",
@@ -172,6 +172,24 @@ if st.session_state.messages[-1]["role"] != "assistant":
       parsed_output = output_parser.parse(ai_dict)
       print("This is Parsed Output:", parsed_output)
 
+      #-------------------------------------------#
+
+      sheet_data = [ {"Annual_Income": parsed_output.annual_income, "Marital_Status": parsed_output.marital_status, 
+                "Cost_of_Living": parsed_output.cost_of_living, "Age": parsed_output.age} ]
+
+      #Establishing a Google Sheets connection
+      conn = st.connection("gsheets", type=GSheetsConnection)
+      data = conn.read(worksheet="Sheet1")
+
+      up_data = conn.update(worksheet="Sheet1", data=sheet_data)
+      del data["Unnamed: 4"]
+      up_data = up_data.dropna(how="all")
+      st.dataframe(up_data)
+
+
+
+    #-------------------------------------------#
+
       #JSON format and Dictionary format output
       process_data(parsed_output) 
 
@@ -194,21 +212,3 @@ with st.sidebar:
             st.write(f"{key.capitalize()}: {value}")
 
 
-
-#-------------------------------------------#
-
-
-
-#Establishing a Google Sheets connection
-# conn = st.connection("gsheets", type=GSheetsConnection)
-# data = conn.read(worksheet="Sheet1")
-
-# del data["Unnamed: 4"]
-# data = data.dropna(how="all")
-# st.dataframe(data)
-
-# conn.update(worksheet="Sheet1", data=data)
-
-
-
-#-------------------------------------------#
